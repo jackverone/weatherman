3d9d711d6d30a83c3a4045b6919c1e40c4fddfb4
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-05-30 13:57:58 +0200

    Small refactor, repackage

--- src/test/java/pl/sda/weatherman/external/api/openweather/service/OpenWeatherApiCurrentWeatherDataServiceTest.java
+++ src/test/java/pl/sda/weatherman/external/api/openweather/service/OpenWeatherApiCurrentWeatherDataServiceTest.java
@@ -1,7 +1,8 @@
-package pl.sda;
+package pl.sda.weatherman.external.api.openweather.service;
 
 import org.junit.jupiter.api.Test;
 import pl.sda.weatherman.external.api.openweather.model.OpenWeatherApiCurrentWeatherDataResponse;
+import pl.sda.weatherman.external.api.openweather.service.OpenWeatherApiCurrentWeatherDataService;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
